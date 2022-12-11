0715416e83c621f359ce8dca6c1215a2f5a31e68
luizalorenco <luiza.lorenco@aluno.unc.br>
2022-12-11 20:47:16 -0300

    atualizar viagem

--- bot/viagensestados.js
+++ bot/viagensestados.js
@@ -1,13 +1,13 @@
 const env = require('../.env')
+const common = require('../common.env')
 const { Telegraf, Markup } = require('telegraf')
 const fetch = require("node-fetch")
 const LocalSession = require('telegraf-session-local')
 const request = require('request')
 
-const regions = ['Sul', 'Norte',  'Nordeste', 'Centro-Oeste', 'Sudeste']
+const regions = common.regions;
+var states = common.states;
 const actions = ['Listar viagens', 'Cadastrar nova viagem',  'Excluir viagem', 'Atualizar viagem']
-
-var states = []
 
 const bot = new Telegraf(env.token)
 
@@ -15,15 +15,7 @@ var apiToken = '';
 
 bot.use(new LocalSession({ database: 'example_db.json' }).middleware())
 
-request(`${env.apiEstados}`, async (err, res, body) => {
-  var response = JSON.parse(body)
-  response.forEach(element => {
-   states.push(element.nome)
-  });
-
-})
-
-async function setapitoken() {
+async function updateAPIToken() {
     var body = {
         email: env.apicredentials.email, 
         password: env.apicredentials.password
@@ -39,9 +31,10 @@ async function setapitoken() {
         .catch (err => console.log(err))
 }
 
-setapitoken();
+updateAPIToken();
 
 async function createTrip(usuario, estado, ano) {
+    updateAPIToken();
     var body = {
         usuario: usuario, 
         estado: estado,
@@ -56,11 +49,29 @@ async function createTrip(usuario, estado, ano) {
             'x-access-token': apiToken
         }
     }).then(res => res.json())
-        .then(json => response = json.token)
+        .then(json => response = json)
         .catch (err => console.log(err))
     return await response;
 }
 
+async function updateTrip(id, ano) {
+    updateAPIToken();
+    var body = {
+        ano: ano
+    }
+    var response;
+    fetch(env.apiBase + '/trips/' + id, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': apiToken
+        }
+    }).then(res => res.json())
+        .then(json => response = json.message)
+        .catch (err => console.log(err))
+    return await response;
+}
 
 bot.start(async ctx => {
     const from = ctx.update.message.from
@@ -73,6 +84,7 @@ bot.start(async ctx => {
 })
 
 bot.hears(regions, async ctx => {
+    updateAPIToken();
     await ctx.reply(` Legal! Para qual estado você viajou? `)
     var message = ctx.message.text
     request(`${env.apiEstados}/${message}`, async (err, res, body) => {
@@ -90,39 +102,14 @@ bot.hears(regions, async ctx => {
 
  
 
-  bot.hears(['Acre',
-    'Alagoas',
-    'Amapá',
-    'Amazonas',
-    'Bahia',
-    'Ceara',
-    'Distrito Federal',
-    'Espírito Santo',
-    'Goiás',
-    'Maranhão',
-    'Mato Grosso',
-    'Mato Grosso do Sul',
-    'Minas Gerais',
-    'Pará',
-    'Paraíba',
-    'Paraná',
-    'Pernambuco',
-    'Piauí',
-    'Rio de Janeiro',
-    'Rio Grande do Norte',
-    'Rio Grande do Sul',
-    'Rondônia',
-    'Roraima',
-    'Santa Catarina',
-    'São Paulo',
-    'Sergipe',
-    'Tocantins'], ctx => {
+bot.hears(states, async ctx => {
     var currentstate = ctx.update.message.text
-    ctx.session.currentstate = currentstate
-    ctx.reply('Interessante! E qual foi o ano dessa viagem?')
-  })
+    ctx.session.currentstate = currentstate;
+    ctx.session.action = 'create';
+    await ctx.reply('Interessante! E qual foi o ano dessa viagem?')
+})
 
-  let list = []
+let list = []
 
   // criando um 'Inline Keyboar' dinâmico
 const itemsButtons = () =>
@@ -135,21 +122,87 @@ const itemsButtons = () =>
 const onlyNumbers = new RegExp('^[0-9]+$')
 
 bot.hears(onlyNumbers, async ctx => {
-    list.push(ctx.update.message.text)
-    console.log(list)
-    var currentstate = ctx.session.currentstate
-    var year = ctx.update.message.text
-    var userID = ctx.update.message.from.id
-
-    await createTrip(userID, currentstate, year)
-
-    ctx.reply(
-        `A viagem para ${currentstate} no ano de ${ctx.update.message.text} foi adicionada à lista! O que deseja fazer agora?`,
-        Markup.keyboard(actions).resize().oneTime(),
-        itemsButtons()
-    )
+    var message = ctx.update.message.text
+    if (message.length === 4) {
+        var action = ctx.session.action;
+    
+        switch (action) {
+            case 'create':
+                list.push(ctx.update.message.text)
+                console.log(list)
+                var currentstate = ctx.session.currentstate
+                var year = ctx.update.message.text
+                var userID = ctx.update.message.from.id
+    
+                await createTrip(userID, currentstate, year)
+    
+                ctx.reply(
+                    `A viagem para ${currentstate} no ano de ${ctx.update.message.text} foi adicionada à lista! O que deseja fazer agora?`,
+                    Markup.keyboard(actions).resize().oneTime(),
+                    itemsButtons()
+                )
+                ctx.session.currentstate = null;
+                ctx.session.action = null;
+                break;
+            case 'update':
+                tripID = ctx.session.currenttrip;
+                var year = ctx.update.message.text
+                await updateTrip(tripID, year);
+                ctx.reply(
+                    `A viagem foi alterada com sucesso! O que deseja fazer agora?`,
+                    Markup.keyboard(actions).resize().oneTime(),
+                    itemsButtons()
+                )
+                ctx.session.currenttrip = null;
+                ctx.session.action = null;
+                break;
+            default:
+                ctx.session.currentstate = null;
+                ctx.session.currenttrip = null;
+                ctx.session.action = null;
+                ctx.reply(
+                    `Não entendi a sua solicitação, o que devemos fazer agora?`,
+                    Markup.keyboard(actions).resize().oneTime(),
+                    itemsButtons()
+                )
+                break;
+        }
+    } else {
+        ctx.reply(
+            `Por gentileza, digite um ano valido`
+        )
+    }
 })
      
+   // removendo os itens da lista quando clicar no botão
+bot.action(/remove (.+)/, ctx => {
+    var id = ctx.match[1]
+
+    fetch(env.apiBase + '/trips/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': apiToken
+        }
+    }).then(res => res.json())
+    .then((json) => {
+
+        ctx.reply(
+            `A viagem foi removida da sua lista!`,
+            Markup.keyboard(actions).resize().oneTime(),
+            itemsButtons()
+        )
+    })
+})
+
+bot.action(/update (.+)/, ctx => {
+    var id = ctx.match[1]
+
+    ctx.session.currenttrip = id;
+    ctx.session.action = 'update';
+
+    ctx.reply('Para qual ano da viagem você gostaria de alterar?')
+})
 
 bot.hears('Cadastrar nova viagem',
 ctx => {
@@ -158,7 +211,7 @@ ctx => {
 )})
   
 bot.hears('Listar viagens',ctx => {
-
+    updateAPIToken();
     var userID = ctx.update.message.from.id
 
     fetch(env.apiBase + '/trips/' + userID, {
@@ -175,25 +228,28 @@ bot.hears('Listar viagens',ctx => {
             json.map(
                 item => Markup.button.callback(item.estado + ' - ' + item.ano, `${item.estado}`)
             ),
-            { columns: 3 }
+            { columns: 2 }
         )
     
         ctx.reply(
-             'Aqui estão todas as suas viagens cadastradas:',
-             myTrips()
-        )
+            'Aqui estão todas as suas viagens cadastradas:',
+            myTrips()
+        ).then(() => {
+            ctx.reply(
+                'O que devemos fazer agora?',
+                Markup.keyboard(actions).resize().oneTime(),
+                itemsButtons()
+            )
+        })
         
     })
     .catch (err => console.log(err))
-
-
     
 })
 
 bot.hears('Excluir viagem',ctx => {
-
-  var userID = ctx.update.message.from.id
-
+    var userID = ctx.update.message.from.id
+  
     fetch(env.apiBase + '/trips/' + userID, {
         method: 'GET',
         headers: {
@@ -203,22 +259,46 @@ bot.hears('Excluir viagem',ctx => {
     }).then(res => res.json())
     .then((json) => {
 
-        const myTrips = () =>
-        Markup.inlineKeyboard(
+        var myTrips = () => Markup.inlineKeyboard(
             json.map(
-                item => Markup.button.callback(item.estado + ' - ' + item.ano, `${item.estado}`)
+                item => Markup.button.callback(item.estado + ' - ' + item.ano, `remove ${item.id}`)
             ),
-            { columns: 3 }
+            { columns: 2 }
         )
+        
 
-  ctx.reply(
-    'Clique sobre uma das viagens de sua lista para excluir. Essa acção não será desfeita.',
-    myTrips(),
-bot.action(/remove (.+)/, ctx => {
-  list = list.filter(item => item !== ctx.match[1])
-/ ctx.reply(`A viagem ${ctx.match[1]} foi removida da sua lista!`, myTrips())
+        ctx.reply(
+            'Clique sobre uma das viagens de sua lista para excluir. Essa acção não será desfeita.',
+            myTrips()
+        )
+    })
 })
-)})
+
+bot.hears('Atualizar viagem',ctx => {
+    var userID = ctx.update.message.from.id
+  
+    fetch(env.apiBase + '/trips/' + userID, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': apiToken
+        }
+    }).then(res => res.json())
+    .then((json) => {
+
+        var myTrips = () => Markup.inlineKeyboard(
+            json.map(
+                item => Markup.button.callback(item.estado + ' - ' + item.ano, `update ${item.id}`)
+            ),
+            { columns: 2 }
+        )
+        
+
+        ctx.reply(
+            'Clique sobre uma das viagens de sua lista para alterar. Essa ação não será desfeita.',
+            myTrips()
+        )
+    })
 })
 
 
